6354869e4ab325f9ab5ba6de2ee8c8bfc55fba03
oomwoo <camera@ovsy.com>
2016-09-02 23:50:44 -0700

    Setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get install libopenblas-dev
+sudo apt-get purge python-numpy
+sudo apt-get install python-dev
+sudo pip install numpy
+sudo apt-get install libopencv-dev python-opencv
+sudo pip install picamera
+sudo apt-get install libyaml-dev libhdf5-dev
+sudo apt-get install imagemagick
+cd ..
+git clone http://github.com/NervanaSystems/neon.git
+cd neon $$ make sysinstall
