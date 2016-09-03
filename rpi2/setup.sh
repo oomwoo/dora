2f072c677874282c0ffa2f804b1b93d46dee9773
oomwoo <camera@ovsy.com>
2016-09-03 00:45:41 -0700

    Moved setup script

---
+++ rpi2/setup.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install python-dev libopenblas-dev libopencv-dev python-opencv libyaml-dev libhdf5-dev
+# sudo apt-get purge python-numpy
+# sudo pip install numpy
+# sudo apt-get install imagemagick
+cd
+git clone http://github.com/NervanaSystems/neon.git
+cd neon $$ sudo make sysinstall
+sudo pip install picamera
