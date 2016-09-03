02009b0ad0a8fd61ebe327640eabc0a83b966bfa
oomwoo <camera@ovsy.com>
2016-09-03 00:19:26 -0700

    Cleaning up

--- setup.sh
+++ setup.sh
@@ -1,12 +1,9 @@
 sudo apt-get update
-sudo apt-get install libopenblas-dev
-sudo apt-get purge python-numpy
-sudo apt-get install python-dev
-sudo pip install numpy
-sudo apt-get install libopencv-dev python-opencv
-sudo pip install picamera
-sudo apt-get install libyaml-dev libhdf5-dev
-sudo apt-get install imagemagick
+sudo apt-get install python-dev libopenblas-dev libopencv-dev python-opencv libyaml-dev libhdf5-dev
+# sudo apt-get purge python-numpy
+# sudo pip install numpy
+# sudo apt-get install imagemagick
 cd ..
 git clone http://github.com/NervanaSystems/neon.git
 cd neon $$ sudo make sysinstall
+sudo pip install picamera
