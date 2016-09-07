7efecec51405c4b5055e29cfec319b1d01380729
Oomwoo <camera@ovsy.com>
2016-09-06 22:40:53 -0700

    I2C setup for rpi3, directory clean-up

--- rpi2/setup.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get update
-sudo apt-get install python-dev libopenblas-dev libopencv-dev python-opencv libyaml-dev libhdf5-dev -y
-# sudo apt-get purge python-numpy
-# sudo pip install numpy
-# sudo apt-get install imagemagick
-cd
-git clone http://github.com/NervanaSystems/neon.git
-cd neon $$ sudo make sysinstall
-sudo pip install picamera
--- rpi3/setup.sh
+++ rpi3/setup.sh
@@ -1,9 +1,10 @@
 sudo apt-get update
 sudo apt-get install python-dev libopenblas-dev libopencv-dev python-opencv libyaml-dev libhdf5-dev -y
-# sudo apt-get purge python-numpy
-# sudo pip install numpy
-# sudo apt-get install imagemagick
 cd
 git clone http://github.com/NervanaSystems/neon.git
 cd neon $$ sudo make sysinstall
-sudo pip install picamera
+sudo apt-get install i2c-tools python-smbus -y
+cd
+git clone https://github.com/adafruit/Adafruit-Motor-HAT-Python-Library.git
+cd Adafruit-Motor-HAT-Python-Library
+sudo python setup.py install
