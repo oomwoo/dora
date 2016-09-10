72de3a114d65b9d1995d648a1bc31c18404d3d6d
Oomwoo <camera@ovsy.com>
2016-09-10 05:39:46 -0700

    Webiopi works, Adafruit motor HAT works, bot base runs

--- rpi3/test/camera_capture_test.py
+++ rpi3/test/camera_capture_test.py
@@ -2,6 +2,7 @@
 
 import picamera
 import picamera.array
+from PIL import Image
 
 
 fps = 90
@@ -15,7 +16,10 @@ camera.exposure_mode = 'fixedfps'
 
 # get camera picture
 stream = picamera.array.PiRGBArray(camera)
-camera.capture(stream, 'rgb', use_video_port=True)
-print(stream.array.shape)
-
+camera.capture(stream, 'rgb', use_video_port=False)
+#camera.capture(stream, 'rgb', use_video_port=True)
 camera.close()
+
+image = Image.fromarray(stream.array)
+image.save("camera_capture_test.png", "PNG")
+image.show()
